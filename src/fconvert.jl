1bea7d3769772150ea1dffb01565afb01c9e0815
Jason Jensen <jjensen@bank-banque-canada.ca>
2022-06-23 16:07:48 -0400

    Refactor and extend fconvert

--- src/fconvert.jl
+++ src/fconvert.jl
@@ -61,6 +61,18 @@ belongs to.
 x = TSeries(2000Q1:2000Q3, collect(Float64, 1:3))
 fconvert(Monthly, x)
 ```
+The optional `values_base` argument determines where to assign values when the
+end period for the lower frequency is partway through a period in the higher frequency.
+There are currently 2 options available: `:end` and `:beginning`. The default is `:end`.
+`:end` means that the output period will hold the value of the input period which corresponds
+to the end of the output period. `:beginning` means the value held will correspond to the value
+at the beginning of the output period.
+For example a Yearly{August} frequency with 20Y = 1, 21Y = 2 to Quarterly.
+With values_base=:end, we would have 20Q2 = 1, 20Q3 = 2, 20Q4 = 2. 
+(End of 20Q3 is September 20Y, which for the Yearly{August} series is in 21Y)
+With values_base=:beginning we would have 20Q2 = 1, 20Q3 = 1, 20Q4 = 2. 
+(Beginning of 20Q3 is July 20Y, which for the Yearly{August} series is in 20Y)
+
 
 ### Converting to Lower Frequency
 The range of the result includes periods that are fully included in the range of
@@ -72,48 +84,97 @@ x = TSeries(2000M1:2000M7, collect(Float64, 1:7))
 fconvert(Quarterly, x; method = :sum)
 ```
 """
-function fconvert(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; method=nothing, orientation=nothing) where {N1,N2}
+function fconvert(F_to::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; method=nothing, values_base=:end) where {N1,N2}
     args = Dict()
     if method !== nothing
         args[:method] = method
     end
-    if orientation !== nothing
-        args[:orientation] = orientation
+    # if values_base !== nothing
+    args[:values_base] = values_base
+    # end
+    N1 > N2 ? _to_higher(F_to, t; args...) : _to_lower(F_to, t; args...)
     end
-    N1 > N2 ? _to_higher(F, t; args...) : _to_lower(F, t; args...)
+
+"""
+fconvert(F1, x::Union{MIT{F2},UnitRange{MIT{F2}}}; round_to=nothing, values_base=nothing) where {F1 <: YPFrequency, F2 <: YPFrequency}
+
+Converts an MIT or a range of MITs to a different YPFrequency.
+
+The optional `values_base` argument is as the conversion for a time series.
+
+The optional `round_to` argument is used in conversions of single MITs to a lower frequency. 
+It determines where to shift the output MIT to in cases where the input MIT is in between periods of the output frequency. 
+The default is :current (provides the output period within which lies provided MIT).
+:next provides the output period following the one in which the provided MIT lands except when the provided MIT is at the start of its current period.
+:previous provides the output period preceeding the one in which the provided MIT lands except when the provided MIT is at the end of its current period.
+
+"""
+function fconvert(F_to::Type{<:YPFrequency{N1}}, Inst_from::Union{<:MIT{<:YPFrequency{N2}},<:UnitRange{<:MIT{<:YPFrequency{N2}}}}; round_to=nothing, values_base=nothing) where {N1,N2}
+    args = Dict()
+    if values_base !== nothing
+        args[:values_base] = values_base
+    end
+    if round_to !== nothing
+        args[:round_to] = round_to
+    end
+    N1 > N2 ? _to_higher(F_to, Inst_from; args...) : _to_lower(F_to, Inst_from; args...)
 end
 
-function _to_higher(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; method=:const, orientation=:end) where {N1,N2}
+
     """
-    NOTE: current const method assumes we are interested in matching end-of-period values.
-    FAME has other approaches (BEGINNING, AVERAGED, SUMMED, ANNUALIZED, FORMULA, HIGH, LOW)
-    These are passed in an "observed" argument to the convert function.
+    _validate_fconvert_yp(F_to, F_from; method) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    This helper function throws errors when conversions are attempted between unsopported YPFrequencies
+    or from/to a YPFrequency with an unsupported reference month.
     """
+function _validate_fconvert_yp(F_to::Type{<:YPFrequency{N1}}, F_from::Type{<:YPFrequency{N2}})  where {N1,N2}
+    if N1 > N2
     (np, r) = divrem(N1, N2)
     if r != 0
         throw(ArgumentError("Cannot convert to higher frequency with $N1 ppy from $N2 ppy - not an exact multiple."))
     end
-    # check if series has an odd anchor
-    shift_length = 0
-    if hasproperty(F, :parameters) && length(F.parameters) > 0
-        # error handling
-        if N1 == 1 && F.parameters[1] ∉ (1,2,3,4,5,6,7,8,9,10,11,12)
-            throw(ArgumentError("Target yearly frequency has an unsuppported end month: $(F.parameters[1]). Must be 1-12."))
+    elseif N2 > N1
+        (np, r) = divrem(N2, N1)
+        if r != 0
+            throw(ArgumentError("Cannot convert to lower frequency with $N1 ppy from $N2 ppy - not an exact multiple."))
         end
-        if N1 == 4 && F.parameters[1] ∉ (1,2,3)
-            throw(ArgumentError("Target quarterly frequency has an unsuppported end month: $(F.parameters[1]). Must be 1-3."))
+        end
+        
+    if hasproperty(F_to, :parameters) && length(F_to.parameters) > 0
+        months_in_period, remainder = divrem(12, N1)
+        if remainder != 0
+            throw(ArgumentError("Cannot convert to frequency with $N1 yearly periods and a non-default end month $(F_to.parameters[1]). 12 must be divisible by The number of yearly periods for custom end months."))
+        end
+        if F_to.parameters[1] ∉ tuple(collect(1:months_in_period)...)
+            throw(ArgumentError("Target yearly frequency has an unsupported end month: $(F_to.parameters[1]). Must be 1-$months_in_period."))
         end
     end
-    if hasproperty(frequencyof(t), :parameters) && length(frequencyof(t).parameters) > 0
-        
-        # error handling
-        if N2 == 1 && frequencyof(t).parameters[1] ∉ (1,2,3,4,5,6,7,8,9,10,11,12)
-            throw(ArgumentError("Yearly frequency has an unsuppported end month: $(frequencyof(t).parameters[1]). Must be 1-12."))
+    if hasproperty(F_from, :parameters) && length(F_from.parameters) > 0
+        months_in_period, remainder = divrem(12, N2)
+        if remainder != 0
+            throw(ArgumentError("Cannot convert from frequency with $N2 yearly periods and a non-default end month $(F_from.parameters[1]). 12 must be divisible by The number of yearly periods for custom end months."))
         end
-        if N2 == 4 && frequencyof(t).parameters[1] ∉ (1,2,3)
-            throw(ArgumentError("Quarterly frequency has an unsuppported end month: $(frequencyof(t).parameters[1]). Must be 1-3."))
+        if F_from.parameters[1] ∉ tuple(collect(1:months_in_period)...)
+            throw(ArgumentError("Source yearly frequency has an unsupported end month: $(F_from.parameters[1]). Must be 1-$months_in_period."))
+        end
+    end
         end
 
+"""
+    _get_shift_to_higher(F_to, F_from; method; values_base=:end, errors=true) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    This helper function returns the number of periods by which the output of a conversion
+    must be shifted down in order to account for reference-month effects of the input and output 
+    frequencies when these are different from the defaults (12 for Yearly and 3 for Quarterly). 
+
+    The optional `errors` argument determines whether to verify if the requested conversion is a valid one.
+
+    For the `values_base` argument see [`fconvert`](@ref)]
+"""
+function _get_shift_to_higher(F_to::Type{<:YPFrequency{N1}}, F_from::Type{<:YPFrequency{N2}}; values_base=:end, errors=true) where {N1,N2}
+    shift_length = 0
+    errors && _validate_fconvert_yp(F_to, F_from)
+    if hasproperty(F_from, :parameters) && length(F_from.parameters) > 0
         """ N1/N2 is ratio between the frequencies, one of  12, 4, or 3
         monthly from yearly: 12/1 = 12
         monthly from quarterly: 12/4 = 3
@@ -160,45 +221,183 @@ function _to_higher(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; m
         
         """
         rounder = floor
-        if (orientation == :beginning)
+        if (values_base == :beginning)
             rounder = ceil
         end
-
-        effective_end_month = frequencyof(t).parameters[1]
-        if hasproperty(F, :parameters) && length(F.parameters) > 0
-            effective_end_month +=  (12/N1) - F.parameters[1]
+        effective_end_month = F_from.parameters[1]
+        if hasproperty(F_to, :parameters) && length(F_to.parameters) > 0
+            effective_end_month +=  (12/N1) - F_to.parameters[1]
         end
         shift_length = Int(N1/N2 - rounder(effective_end_month / (12 / N1) ))
     end
+    return shift_length
+end
+
+"""
+    _get_shift_to_lower(F_to, F_from; method) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    This helper function returns the number of periods by which the input of a conversion
+    must be shifted up in order to account for reference-month effects of the input and output 
+    frequencies when these are different from the defaults (12 for Yearly and 3 for Quarterly). 
+
+    The optional `errors` argument determines whether to verify if the requested conversion is a valid one.
+"""
+function _get_shift_to_lower(F_to::Type{<:YPFrequency{N1}}, F_from::Type{<:YPFrequency{N2}}; errors=true) where {N1,N2}
+    errors && _validate_fconvert_yp(F_to, F_from)
     
+    shift_length = 0
+    if hasproperty(F_to, :parameters) && length(F_to.parameters) > 0
+        # in this case we need to shift the index ranges
+        shift_length += Int((12/N1) - F_to.parameters[1])
+    end
+    return shift_length
+end
+
+"""
+    _to_higher(F_to, MIT{F_from}; values_base=:end, errors=true) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    Convert a MIT to a higher frequency. 
+
+    The optional `errors` argument determines whether to verify if the requested conversion is a valid one.
+
+    For the `values_base` argument see [`fconvert`](@ref)]
+"""
+# yearly to quarterly
+function _to_higher(F_to::Type{<:YPFrequency{N1}}, MIT_from::MIT{<:YPFrequency{N2}}; values_base=:end, errors=true, args...) where {N1,N2}
+    errors && _validate_fconvert_yp(F_to, frequencyof(MIT_from))
+    (np, r) = divrem(N1, N2)
+    shift_length = _get_shift_to_higher(F_to, frequencyof(MIT_from); values_base=values_base, errors=false)
     # np = number of periods of the destination frequency for each period of the source frequency
-    (y1, p1) = mit2yp(firstindex(t))
-    # (y2, p2) = yp(lastindex(t))
-    fi = MIT{F}(y1, (p1 - 1) * np + 1)
+    (y1, p1) = mit2yp(MIT_from)
+    fi = MIT{F_to}(y1, (p1 - 1) * np + 1) - shift_length
+    return fi
+        end
+
+"""
+    _to_higher(F_to, UnitRange{MIT{F_from}}; values_base=:end, errors=true) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    Convert a MIT range to a higher frequency. 
+
+    The optional `errors` argument determines whether to verify if the requested conversion is a valid one.
+
+    For the `values_base` argument see [`fconvert`](@ref)]
+"""
+function _to_higher(F_to::Type{<:YPFrequency{N1}}, range::UnitRange{<:MIT{<:YPFrequency{N2}}}; values_base=:end, errors=true, args...) where {N1,N2}
+    errors && _validate_fconvert_yp(F_to, frequencyof(first(range)))
+    (np, r) = divrem(N1, N2)
+    fi = _to_higher(F_to, first(range), values_base=values_base, errors=false)
+    li = fi + np*length(range) - 1
+    return fi:li
+    end
+    
+"""
+    _to_higher(F_to, TSeries{MIT{F_from}}; method=:const, values_base=:end, errors=true) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    Convert a TSeries to a higher frequency. 
+
+    The optional `errors` argument determines whether to verify if the requested conversion is a valid one.
+
+    For the `values_base` argument see [`fconvert`](@ref)]
+"""
+function _to_higher(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; method=:const, values_base=:end, errors=true, args...) where {N1,N2}
+    """
+    NOTE: current const method assumes we are interested in matching end-of-period values.
+    FAME has other approaches (BEGINNING, AVERAGED, SUMMED, ANNUALIZED, FORMULA, HIGH, LOW)
+    These are passed in an "observed" argument to the convert function.
+    """
+    errors && _validate_fconvert_yp(F, frequencyof(t))
+    (np, r) = divrem(N1, N2)
+
+    # np = number of periods of the destination frequency for each period of the source frequency
+    fi = _to_higher(F, firstindex(t), values_base=values_base, errors=false)
+
     # lastindex_s = pp(y2, p2*np; N=N1))
     if method == :const
-        if shift_length != 0
-            shift!(TSeries(fi, repeat(t.values, inner=np)), shift_length)
-        else
         return TSeries(fi, repeat(t.values, inner=np))
-        end
     else
         throw(ArgumentError("Conversion method not available: $(method)."))
     end
 end
 
-function _to_lower(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; method=:mean) where {N1,N2}
+"""
+    _to_lower(F_to, MIT{F_from}; values_base=:beginning, errors=true, remainder=false) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    Convert a MIT to a lower frequency. 
+
+    The optional `round_to` argument determines where to shift the output MIT to in cases where the input MIT is in between periods of the 
+    output frequency. The default is :none (provides the output period within which lies provided MIT).
+    :next provides the output period following the one in which the provided MIT lands except when the provided MIT is at the start of its current period.
+    :previous provides the output period preceeding the one in which the provided MIT lands except when the provided MIT is at the end of its current period.
+
+    Examples:
+    _to_lower(Quarterly, 20M2, round_to=:current) = 20Q1
+    _to_lower(Quarterly, 20M2, round_to=:next) = 20Q2
+    _to_lower(Quarterly, 20M2, round_to=:previous) = 19Q4
+    _to_lower(Quarterly, 20M3, round_to=:previous) = 20Q1
+    _to_lower(Quarterly, 20M1, round_to=:next) = 20Q1
+
+    The optional `errors` argument determines whether to verify if the requested conversion is a valid one.
+
+    When passed the optional `remainder` argument as `true`, the function returns both the output period, and the remaining number of input periods not covered by the conversion.
+    Default is `false`.
+"""
+# monthly to quarterly
+function _to_lower(F_to::Type{<:YPFrequency{N1}}, MIT_from::MIT{<:YPFrequency{N2}}; round_to=:current, errors=true, remainder=false, args...) where {N1,N2}
+    errors && _validate_fconvert_yp(F_to, frequencyof(MIT_from))
     (np, r) = divrem(N2, N1)
-    # println("np = $np, r = $r")
-    if r != 0
-        throw(ArgumentError("Cannot convert to lower frequency with $N1 from $N2 - not an exact multiple."))
+    shift_length = _get_shift_to_lower(F_to, frequencyof(MIT_from), errors=false)
+    (y1, p1) = mit2yp(MIT_from + shift_length)
+    (d1, r1) = divrem(p1 - 1, np)
+    mit = nothing
+    if round_to == :next
+        mit = MIT{F_to}(y1, d1 + 1) + (r1 > 0)
+    elseif round_to == :previous
+        mit = MIT{F_to}(y1, d1 + 1) - (r1 < np - 1)
+    elseif round_to == :current
+        mit = MIT{F_to}(y1, d1 + 1)
+    else
+        throw(ArgumentError("Unknown round_to: $(round_to). Should be :next or :previous, or :current."))
     end
-    if hasproperty(frequencyof(t), :parameters) && length(frequencyof(t).parameters) > 0
-        # error handling
-        if N2 == 4 && frequencyof(t).parameters[1] ∉ (1,2,3)
-            throw(ArgumentError("Quarterly input frequency has an unsuppported end month: $(frequencyof(t).parameters[1]). Must be 1-3."))
+    if remainder
+        return mit, r1
+    end
+    return mit
         end
 
+"""
+    _to_lower(F_to, UnitRange{MIT{F_from}}; errors=true, warning=true) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    Convert a MIT range to a lower frequency. 
+"""
+function _to_lower(F_to::Type{<:YPFrequency{N1}}, range::UnitRange{<:MIT{<:YPFrequency{N2}}}; errors=true, warnings=true, args...) where {N1,N2}
+    errors && _validate_fconvert_yp(F_to, frequencyof(first(range)))
+    F_from = frequencyof(range)
+    if hasproperty(F_from, :parameters) && length(F_from.parameters) > 0
+        #quarterly{1} to yearly
+        """
+        All the specialized YPFrequencies (Yearly{N}, Quarterly{N}) are based on some monthly shift.
+        We therefore convert the series to monthly, before recalling fconvert on the output.
+        """
+        return fconvert(F_to, fconvert(Monthly, range))
+    end
+    fi, r1 = _to_lower(F_to, first(range); round_to=:next, errors=false, remainder=true)
+    li, r2 = _to_lower(F_to, last(range); round_to=:previous, errors=false, remainder=true)
+    if (r1 != 0 || r2 != 0) && warnings
+        @warn "Range conversionfrom $(range) to $(F_to) has remainders: $r1, $r2."
+        end
+    return fi:li
+        end
+
+"""
+    _to_lower(F_to, TSeries{MIT{F_from}}; method=:mean, errors=true) where {F_to <: YPFrequency, F_from <: YPFrequency}
+
+    Convert a TSeries to a lower frequency. 
+"""
+function _to_lower(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; method=:mean, errors=true, args...) where {N1,N2}
+    F_from = frequencyof(t)
+    errors && _validate_fconvert_yp(F, F_from)
+    (np, r) = divrem(N2, N1)
+    if hasproperty(F_from, :parameters) && length(F_from.parameters) > 0
         #quarterly{1} to yearly
         """
         All the specialized YPFrequencies (Yearly{N}, Quarterly{N}) are based on some monthly shift.
@@ -206,30 +405,10 @@ function _to_lower(F::Type{<:YPFrequency{N1}}, t::TSeries{<:YPFrequency{N2}}; me
         """
         return fconvert(F, fconvert(Monthly, t), method=method)
     end
-    shift_length = 0
-    if hasproperty(F, :parameters) && length(F.parameters) > 0
-        # error handling
-        if N1 == 1 && F.parameters[1] ∉ (1,2,3,4,5,6,7,8,9,10,11,12)
-            throw(ArgumentError("Target yearly frequency has an unsuppported end month: $(F.parameters[1]). Must be 1-12."))
-        end
-        if N1 == 4 && F.parameters[1] ∉ (1,2,3)
-            throw(ArgumentError("Target quarterly frequency has an unsuppported end month: $(F.parameters[1]). Must be 1-3."))
-        end
-
-        # in this case we need to shift the index ranges
-        shift_length += Int((12/N1) - F.parameters[1])
-
-    end
-
-    (y1, p1) = mit2yp(firstindex(t) + shift_length)
-    (d1, r1) = divrem(p1 - 1, np)
-    fi = MIT{F}(y1, d1 + 1) + (r1 > 0)
-    # println("y1 = $y1, p1 = $p1, d1 = $d1, r1 = $r1, fi = $fi")
-    (y2, p2) = mit2yp(lastindex(t) + shift_length)
-    (d2, r2) = divrem(p2 - 1, np)
-    li = MIT{F}(y2, d2 + 1) - (r2 < np - 1)
-    # println("y2 = $y2, p2 = $p2, d2 = $d2, r2 = $r2, li = $li")
+    fi, r1 = _to_lower(F, t.firstdate; round_to=:next, errors=false, remainder=true)
+    li, r2 = _to_lower(F, last(rangeof(t)); round_to=:previous, errors=false, remainder=true)
     vals = t[begin+(r1>0)*(np-r1):end-(r2<np-1)*(1+r2)].values
+    
     # println("vals = $vals")
     if method == :mean
         ret = mean(reshape(vals, np, :); dims=1)
--- test/test_fconvert.jl
+++ test/test_fconvert.jl
@@ -1,3 +1,5 @@
+using Suppressor
+
 
 @testset "fconvert, general" begin
     t = TSeries(5U, collect(1:10))
@@ -31,10 +33,6 @@
         # @test rangeof(fconvert(Quarterly, TSeries(1M1 .+ (0:47+i)))) == 1Y:4Y #current output is 1Q1:4Q4
     end
 
-    #non-user called functions
-    @test_throws ArgumentError TimeSeriesEcon._to_lower(Monthly, q)
-    @test_throws ArgumentError TimeSeriesEcon._to_higher(Yearly, q)
-
     #wrong method for conversion direction
     @test_throws ArgumentError fconvert(Monthly, q, method=:mean)
     @test_throws ArgumentError fconvert(Yearly, q, method=:const)
@@ -49,35 +47,62 @@ end
     q1 = fconvert(Quarterly, y1)
     @test rangeof(q1) == 22Q1:23Q4;
     @test q1.values == [1,1,1,1,2,2,2,2]
-    q1_beginning = fconvert(Quarterly, y1, orientation=:beginning)
+    q1_beginning = fconvert(Quarterly, y1, values_base=:beginning)
     @test rangeof(q1_beginning) == 22Q1:23Q4;
     @test q1_beginning.values == [1,1,1,1,2,2,2,2]
-
+    r1 = fconvert(Quarterly, rangeof(y1), values_base=:beginning)
+    @test r1 == 22Q1:23Q4;
+    mit1_start = fconvert(Quarterly, y1.firstdate, values_base=:beginning)
+    @test mit1_start == 22Q1;
+    
     y2 = TSeries(MIT{Yearly{7}}(22), [1,2])
     q2 = fconvert(Quarterly, y2)
     @test rangeof(q2) ==21Q3:23Q2;
     @test q2.values == [1,1,1,1,2,2,2,2]
-    q2_beginning = fconvert(Quarterly, y2; orientation=:beginning)
+    q2_beginning = fconvert(Quarterly, y2; values_base=:beginning)
     @test rangeof(q2_beginning) ==  21Q4:23Q3;
     @test q2_beginning.values == [1,1,1,1,2,2,2,2]
+    r2 = fconvert(Quarterly, rangeof(y2); values_base=:beginning)
+    @test r2 == 21Q4:23Q3
+    mit2_start = fconvert(Quarterly, y2.firstdate, values_base=:beginning)
+    @test mit2_start == 21Q4;
 
     y3 = TSeries(MIT{Yearly{7}}(22), [1,2])
     q3 = fconvert(Quarterly{1}, y3)
     @test rangeof(q3) ==  MIT{Quarterly{1}}(21*4+3):MIT{Quarterly{1}}(23*4+2) # 21Q4:23Q3;
     @test q3.values == [1,1,1,1,2,2,2,2]
-    q3_beginning = fconvert(Quarterly{1}, y3; orientation=:beginning)
+    r3 = fconvert(Quarterly{1}, rangeof(y3))
+    @test r3 == MIT{Quarterly{1}}(21*4+3):MIT{Quarterly{1}}(23*4+2) #21Q4:23Q3
+    mit3_start = fconvert(Quarterly{1}, y3.firstdate)
+    @test mit3_start == MIT{Quarterly{1}}(21*4+3); #21Q4
+    q3_beginning = fconvert(Quarterly{1}, y3; values_base=:beginning)
     @test rangeof(q3_beginning) ==  MIT{Quarterly{1}}(21*4+3):MIT{Quarterly{1}}(23*4+2) # 21Q4:23Q3;
     @test q3_beginning.values == [1,1,1,1,2,2,2,2]
+    r3_beginning = fconvert(Quarterly{1}, rangeof(y3); values_base=:beginning)
+    @test r3_beginning == MIT{Quarterly{1}}(21*4+3):MIT{Quarterly{1}}(23*4+2) #21Q4:23Q3
+    mit3_start_beginning = fconvert(Quarterly{1}, y3.firstdate, values_base=:beginning)
+    @test mit3_start_beginning == MIT{Quarterly{1}}(21*4+3); #21Q4
 
     y4 = TSeries(MIT{Yearly}(22), [1,2])
     m1 = fconvert(Monthly, y4)
     @test rangeof(m1) == 22M1:23M12;
     @test m1.values == [repeat([1], 12)..., repeat([2], 12)...]
+    r4 = fconvert(Monthly, rangeof(y4))
+    @test r4 == 22M1:23M12;
+    mit4_start = fconvert(Monthly, y4.firstdate)
+    @test mit4_start == 22M1;
 
     y5 = TSeries(MIT{Yearly{7}}(22), [1,2])
     m2 = fconvert(Monthly, y5)
     @test rangeof(m2) ==21M8:23M7;
     @test m2.values == [repeat([1], 12)..., repeat([2], 12)...]
+    r5 = fconvert(Monthly, rangeof(y5))
+    @test r5 == 21M8:23M7;
+    mit5_start = fconvert(Monthly, y5.firstdate)
+    @test mit5_start == 21M8;
+
+
+    # need one with orientation = :end != orientation = :end
 
     
 
@@ -88,41 +113,111 @@ end
     y1 = fconvert(Yearly, q1)
     @test rangeof(y1) == 2Y:4Y
     @test y1.values == [3, 5, 7]
+    @suppress begin
+        r1 = fconvert(Yearly, rangeof(q1))
+        @test r1 == 2Y:4Y
+    end
+    mit1_start = fconvert(Yearly, q1.firstdate, round_to=:next)
+    @test mit1_start == 2Y;
+    mit1_end = fconvert(Yearly, last(rangeof(q1)), round_to=:previous)
+    @test mit1_end == 4Y;
+    
 
     q2 = TSeries(MIT{Quarterly{2}}(5), [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8])
     y2 = fconvert(Yearly, q2)
     @test rangeof(y2) == 2Y:4Y
     @test y2.values == [3 + 1/6, 5 + 1/6, 7 + 1/6]
+    @suppress begin
+        r2 = fconvert(Yearly, rangeof(q2))
+        @test r2 == 2Y:4Y
+    end
+    mit2_start = fconvert(Yearly, q2.firstdate, round_to=:next)
+    @test mit2_start == 2Y;
+    mit2_end = fconvert(Yearly, last(rangeof(q2)), round_to=:previous)
+    @test mit2_end == 4Y;
 
     q3 = TSeries(1Q2, [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8])
     y3 = fconvert(Yearly, q3)
     @test rangeof(y3) == 2Y:4Y
     @test y3.values == [3, 5, 7]
+    @suppress begin
+        r3 = fconvert(Yearly, rangeof(q3))
+        @test r3 == 2Y:4Y
+    end
+    mit3_start = fconvert(Yearly, q3.firstdate, round_to=:next)
+    @test mit3_start == 2Y;
+    mit3_end = fconvert(Yearly, last(rangeof(q3)), round_to=:previous)
+    @test mit3_end == 4Y;
 
     q4 = TSeries(MIT{Quarterly{2}}(5), [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8])
     y4 = fconvert(Yearly, q4)
     @test rangeof(y4) == 2Y:3Y
     @test y4.values == [3 + 1/6, 5 + 1/6]
+    @suppress begin
+        r4 = fconvert(Yearly, rangeof(q4))
+        @test r4 == 2Y:3Y
+    end
+    mit4_start = fconvert(Yearly, q4.firstdate, round_to=:next)
+    @test mit4_start == 2Y;
 
     m5 = TSeries(20M1, collect(1:36))
     y5 = fconvert(Yearly, m5)
     @test rangeof(y5) == 20Y:22Y
     @test y5.values == [6.5, 18.5, 30.5]
+    @suppress begin
+        r5 = fconvert(Yearly, rangeof(m5))
+        @test r5 == 20Y:22Y
+    end
+    mit5_start = fconvert(Yearly, m5.firstdate, round_to=:next)
+    @test mit5_start == 20Y;
+    mit5_end = fconvert(Yearly, last(rangeof(m5)), round_to=:previous)
+    @test mit5_end == 22Y;
 
     m6 = TSeries(20M1, collect(1:36))
     y6 = fconvert(Yearly{9}, m6)
     @test rangeof(y6) == MIT{Yearly{9}}(21):MIT{Yearly{9}}(22) #20Y:21Y
     @test y6.values == [15.5, 27.5]
+    @suppress begin
+        r6 = fconvert(Yearly{9}, rangeof(m6))
+        @test r6 == MIT{Yearly{9}}(21):MIT{Yearly{9}}(22) #20Y:21Y
+    end
+    mit6_start = fconvert(Yearly{9}, m6.firstdate, round_to=:next)
+    @test mit6_start == MIT{Yearly{9}}(21); #20Y:21Y
+    mit6_end = fconvert(Yearly{9}, last(rangeof(m6)), round_to=:previous)
+    @test mit6_end == MIT{Yearly{9}}(22); #20Y:21Y
 
     m7 = TSeries(20M1, collect(1:36))
     q7 = fconvert(Quarterly, m7)
-    @test rangeof(q7) == 20Q1:22Q4 #20Y:21Y
+    @test rangeof(q7) == 20Q1:22Q4
     @test q7.values == collect(2:3:35)
+    @suppress begin
+        r7 = fconvert(Quarterly, rangeof(m7))
+        @test r7 == 20Q1:22Q4
+    end
+    mit7_start = fconvert(Quarterly, m7.firstdate, round_to=:next)
+    @test mit7_start == 20Q1;
+    mit7_end = fconvert(Quarterly, last(rangeof(m7)), round_to=:previous)
+    @test mit7_end == 22Q4;
 
     m8 = TSeries(20M1, collect(1:36))
     q8 = fconvert(Quarterly{2}, m8)
     @test rangeof(q8) == MIT{Quarterly{2}}(20*4 + 1):MIT{Quarterly{2}}(22*4 + 3) #20Q2:22Q4
     @test q8.values == collect(4:3:34)
+    @suppress begin
+        r8 = fconvert(Quarterly{2}, rangeof(m8))
+        @test r8 == MIT{Quarterly{2}}(20*4 + 1):MIT{Quarterly{2}}(22*4 + 3) #20Q2:22Q4
+    end
+    mit8_start = fconvert(Quarterly{2}, m8.firstdate, round_to=:next)
+    @test mit8_start == MIT{Quarterly{2}}(20*4 + 1) # 20Q2
+    mit8_end = fconvert(Quarterly{2}, last(rangeof(m8)), round_to=:previous)
+    @test mit8_end == MIT{Quarterly{2}}(22*4 + 3) #22Q4
+
+    # bias in single period conversions
+    @test TimeSeriesEcon._to_lower(Quarterly, 20M2, round_to=:current) == 20Q1
+    @test TimeSeriesEcon._to_lower(Quarterly, 20M2, round_to=:next) == 20Q2
+    @test TimeSeriesEcon._to_lower(Quarterly, 20M2, round_to=:previous) == 19Q4
+    @test TimeSeriesEcon._to_lower(Quarterly, 20M3, round_to=:previous) == 20Q1
+    @test TimeSeriesEcon._to_lower(Quarterly, 20M1, round_to=:next) == 20Q1
     
     """
     FAME reproduction scripts
