8423eae6f4f1d2b9682001df9f0583e3cb4b868c
Boyan Bejanov <bbejanov@gmail.com>
2022-09-22 13:26:45 -0400

    adjoint of MVTSeries handled like a matrix + AbstractUnitRange so it works for Base.OneTo as well

--- src/linalg.jl
+++ src/linalg.jl
@@ -7,16 +7,22 @@
 import Base: *, \, /
 
 for op in (:*, :\, :/)
+    for AT in (AbstractMatrix, AbstractVector)
+        for ST in (MVTSeries, TSeries)
+            eval(quote
+                $op(A::$AT, B::$ST) = $op(A, _vals(B))
+                $op(A::$ST, B::$AT) = $op(_vals(A), B)
+            end)
+        end
+    end
     eval(quote
-        $op(A::AbstractVecOrMat, B::TSeries) = $op(A, _vals(B))
-        $op(A::AbstractVecOrMat, B::MVTSeries) = $op(A, _vals(B))
-        $op(A::TSeries, B::AbstractVecOrMat) = $op(_vals(A), B)
-        $op(A::MVTSeries, B::AbstractVecOrMat) = $op(_vals(A), B)
         $op(A::TSeries, B::TSeries) = $op(_vals(A), _vals(B))
         $op(A::TSeries, B::MVTSeries) = $op(_vals(A), _vals(B))
         $op(A::MVTSeries, B::TSeries) = $op(_vals(A), _vals(B))
         $op(A::MVTSeries, B::MVTSeries) = $op(_vals(A), _vals(B))
     end)
-
 end
+
+Base.adjoint(A::MVTSeries) = adjoint(_vals(A))
+Base.adjoint(A::TSeries) = adjoint(_vals(A))
 
