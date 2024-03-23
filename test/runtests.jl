25ec08876cc51b5344ef4447397307bea1c5e7fe
Jason Jensen <jjensen@bank-banque-canada.ca>
2024-03-22 21:45:42 -0400

    x13 re-enable other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,18 +7,18 @@ using Statistics
 using Random
 using Suppressor
 
-# include("test_mit.jl")
-# include("test_tseries.jl")
-# include("test_business.jl")
-# include("test_mvtseries.jl")
-# include("test_workspace.jl")
-# include("test_fconvert.jl")
-# include("test_serialize.jl")
-# include("test_various.jl")
-# include("test_22.jl")
+include("test_mit.jl")
+include("test_tseries.jl")
+include("test_business.jl")
+include("test_mvtseries.jl")
+include("test_workspace.jl")
+include("test_fconvert.jl")
+include("test_serialize.jl")
+include("test_various.jl")
+include("test_22.jl")
 include("test_x13spec.jl")
 if !Sys.isapple()
     include("test_x13run.jl")
 end
 
-# include("test_dataecon.jl")
+include("test_dataecon.jl")
