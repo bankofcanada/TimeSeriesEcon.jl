9225607ddac86e2eee8509f475883580ea56400f
Jason Jensen <jjensen@bank-banque-canada.ca>
2025-03-11 12:26:29 -0400

    properly cleanup changes

--- src/workspaces.jl
+++ src/workspaces.jl
@@ -65,14 +65,6 @@ MacroTools.@forward Workspace._c (Base.isempty, Base.keys, Base.haskey, Base.val
 MacroTools.@forward Workspace._c (Base.iterate, Base.get, Base.get!,)
 
 function Base.eltype(w::AbstractWorkspace)
-    ET = isempty(w) ? Any : try
-        Base.promote_typeof(values(w)...)
-    catch
-        Any
-    end
-    return Pair{Symbol,ET}
-end
-function Base.eltype(w::AbstractWorkspace)
     ET = isempty(w) ? Any : reduce(Base.promote_typeof, values(w))
     return Pair{Symbol,ET}
 end
